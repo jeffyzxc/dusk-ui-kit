9172ae3f0b2da8b1aa93ad5b40c63bbeca8cfbc2
Kieran Hall <kieran.hall@gmail.com>
2020-02-04 09:38:51 +0100

    Fixed tests

--- src/components/organisms/paging-table/PagingTable.test.js
+++ src/components/organisms/paging-table/PagingTable.test.js
@@ -3,7 +3,29 @@ import renderer from 'react-test-renderer'
 
 import PagingTable from './PagingTable'
 
+const data = [
+  {
+    id: 1,
+    name: 'Dusk Chicken',
+    price: '$1000',
+  },
+]
+const columns = [
+  {
+    dataField: 'id',
+    text: 'Product ID',
+  },
+  {
+    dataField: 'name',
+    text: 'Product Name',
+  },
+  {
+    dataField: 'price',
+    text: 'Product Price',
+  },
+]
+
 it('test to see if the Header renders correctly', () => {
-  const tree = renderer.create(<PagingTable />).toJSON()
+  const tree = renderer.create(<PagingTable data={data} columns={columns} />).toJSON()
   expect(tree).toMatchSnapshot()
 })
