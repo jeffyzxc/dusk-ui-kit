265e5be2483f7e196bd1e3b147e681ec47ded7b6
Kieran Hall <kieran.hall@gmail.com>
2020-03-05 01:24:14 +0100

    Added start for Drawer component.

---
+++ packages/organisms/drawer/Drawer.stories.js
@@ -0,0 +1,34 @@
+import {
+  withKnobs,
+  select,
+  optionsKnob as options
+} from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import { variants } from "@dusk/helpers";
+import DefaultView from "./storybook-views/Default.svelte";
+import readme from "./readme.stories.md";
+import "@dusk/styles/tailwind.css";
+
+export default {
+  title: "Components/Organisims/Drawer",
+  parameters: {
+    notes: readme
+  },
+  decorators: [withKnobs]
+};
+
+export const Default = () => ({
+  Component: DefaultView,
+  props: {
+    variant: select(
+      "Drawer Variant",
+      variants.ORGANISM.DRAWER,
+      variants.ORGANISM.DRAWER.BRAND
+    ),
+    logoVariant: select(
+      "Logo Variant",
+      variants.ATOM.LOGO,
+      variants.ATOM.LOGO.WHITE
+    )
+  }
+});
---
+++ packages/organisms/drawer/storybook-views/Default.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Drawer from "../";
+  import Menu from "@dusk/menu";
+  import List, { Item } from "@dusk/list";
+  import Logo from "@dusk/logo";
+  export let variant;
+  export let logoVariant;
+</script>
+
+<Drawer {variant}>
+  <span slot="logo">
+    <Logo variant="{logoVariant}" class="w-48" />
+  </span>
+  <Menu>
+    <List>
+      <Item href="#test">Dashboard</Item>
+      <Item>
+        Wallet
+        <List>
+          <Item href="#test">Send</Item>
+          <Item href="#test">Recieve</Item>
+        </List>
+      </Item>
+    </List>
+  </Menu>
+  <Menu>
+    <List>
+      <Item href="#test">Help</Item>
+      <Item href="#test">Settings</Item>
+    </List>
+  </Menu>
+</Drawer>
