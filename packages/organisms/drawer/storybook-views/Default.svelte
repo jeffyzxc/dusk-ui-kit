b34b463cc9db163ed8eae814fe4bbbb919926a29
Kieran Hall <kieran.hall@gmail.com>
2020-03-09 18:35:17 +0100

    Added icon example

--- packages/organisms/drawer/storybook-views/Default.svelte
+++ packages/organisms/drawer/storybook-views/Default.svelte
@@ -3,30 +3,47 @@
   import Menu from "@dusk/menu";
   import List, { Item } from "@dusk/list";
   import Logo from "@dusk/logo";
+  import Icon from "@dusk/icon";
   export let variant;
   export let logoVariant;
 </script>
 
 <Drawer {variant}>
-  <span slot="logo">
+  <div slot="logo">
     <Logo variant="{logoVariant}" class="w-48" />
-  </span>
+  </div>
   <Menu>
     <List>
-      <Item href="#test">Dashboard</Item>
+      <Item href="#test">
+        <Icon name="view-dashboard-outline" />
+        Dashboard
+      </Item>
       <Item>
+        <Icon name="wallet" />
         Wallet
         <List>
-          <Item href="#test">Send</Item>
-          <Item href="#test">Recieve</Item>
+          <Item href="#test">
+            <Icon name="arrow-right-circle" />
+            Send
+          </Item>
+          <Item href="#test">
+            <Icon name="arrow-left-circle" />
+            Recieve
+          </Item>
         </List>
       </Item>
     </List>
   </Menu>
   <Menu>
     <List>
-      <Item href="#test">Help</Item>
-      <Item href="#test">Settings</Item>
+      <Item href="#test">
+        <Icon name="help-circle-outline" />
+        Help
+      </Item>
+      <Item href="#test">
+        <Icon name="cog-outline" />
+        Settings
+      </Item>
     </List>
   </Menu>
 </Drawer>
