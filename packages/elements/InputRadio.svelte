4e0669758807f82cfc46affb9a661ad7e631b5fa
Essam Dawood <essam_d@hotmail.com>
2021-04-13 12:37:28 +0200

    docs(input-radio): 📚️ created input-radio
    affects: @dusk-network/elements
    
    specifically targets the radio element type

---
+++ packages/elements/InputRadio.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { current_component } from "svelte/internal";
+  import { forwardEventsBuilder, exclude, useActions } from "@dusk-network/helpers";
+  const forwardEvents = forwardEventsBuilder(current_component);
+  export let use = [];
+  export let id = "";
+  let className = "";
+  export { className as class };
+</script>
+
+<input
+  use:useActions="{use}"
+  use:forwardEvents
+  class="duk-radio {className}"
+  type="radio"
+  id="{id}"
+  {...exclude($$props, ['use', 'class'])} />
