7e8885ec059674598f99d3f38627242f1cc94c10
Essam Dawood <essam_d@hotmail.com>
2021-04-14 11:36:18 +0200

    docs(logo): 📚️ removed the styling from the default file
    affects: @dusk-network/logo

--- packages/atoms/logo/storybook-views/Default.svelte
+++ packages/atoms/logo/storybook-views/Default.svelte
@@ -4,6 +4,4 @@
   export let href;
 </script>
 
-<div class="p-10 w-6/12 mx-auto bg-purple-100 text-center">
-  <Logo variant="{variant}" href="{href}" style="width: 305px" />
-</div>
+<Logo variant="{variant}" href="{href}" style="width: 305px" />
