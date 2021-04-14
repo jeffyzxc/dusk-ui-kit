eea49c976796136efb5fe4052f16e073bc3c80dd
Essam Dawood <essam_d@hotmail.com>
2021-04-14 11:38:00 +0200

    docs(radio): 📚️ removed the styling from the default file
    affects: @dusk-network/radio

--- packages/atoms/radio/storybook-views/Default.svelte
+++ packages/atoms/radio/storybook-views/Default.svelte
@@ -3,7 +3,5 @@
   export let variant;
 </script>
 
-<div class="p-10 w-6/12 mx-auto bg-purple-200 text-center">
-  <Radio variant="{variant}" name="radio" id="radio_1" class="p-2">Yo! I am a Radio</Radio>
-  <Radio variant="{variant}" name="radio" id="radio_2" class="p-2">Yo! I am a Radio too</Radio>
-</div>
+<Radio variant="{variant}" name="radio" id="radio_1">Yo! I am a Radio</Radio>
+<Radio variant="{variant}" name="radio" id="radio_2">Yo! I am a Radio too</Radio>
