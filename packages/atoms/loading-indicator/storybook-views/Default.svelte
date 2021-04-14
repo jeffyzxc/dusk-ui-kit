338913d84baadcd40882e963fcd5cdd303124828
Essam Dawood <essam_d@hotmail.com>
2021-04-14 11:35:06 +0200

    docs(loading-indicator): 📚️ removed the styling from the default file
    affects: @dusk-network/loading-indicator

--- packages/atoms/loading-indicator/storybook-views/Default.svelte
+++ packages/atoms/loading-indicator/storybook-views/Default.svelte
@@ -3,6 +3,4 @@
   export let variant;
 </script>
 
-<div class="p-10 w-6/12 mx-auto bg-purple-100 text-center">
-  <LoadingIndicator variant="{variant}" class="w-48" />
-</div>
+<LoadingIndicator variant="{variant}" class="w-48" />
