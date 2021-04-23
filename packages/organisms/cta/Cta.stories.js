4097e9371c471c834a12a155f9dceed3f1514667
Essam Dawood <essam_d@hotmail.com>
2021-04-23 10:51:38 +0200

    docs(cta): 📚️ updated using args and argtypes
    affects: @dusk-network/cta

--- packages/organisms/cta/Cta.stories.js
+++ packages/organisms/cta/Cta.stories.js
@@ -1,20 +1,48 @@
-import { withKnobs, text } from "@storybook/addon-knobs";
-import DefaultView from "./storybook-views/Default.svelte";
-import readme from "./readme.stories.md";
+// import { withKnobs, text } from "@storybook/addon-knobs";
+// import DefaultView from "./storybook-views/Default.svelte";
+// import readme from "./readme.stories.md";
+
+// export default {
+//   title: "Components/Organisms/CTA",
+//   parameters: {
+//     notes: readme,
+//   },
+//   decorators: [withKnobs],
+// };
+
+// export const Default = () => ({
+//   Component: DefaultView,
+//   props: {
+//     heading: text("CTA Heading", "Call to action heading..."),
+//     href: text("CTA Link", "#"),
+//     buttonText: text("CTA Button Text", "The cake is a lie"),
+//   },
+// });
+
+import Default from "./storybook-views/Default.svelte";
+
+import "@dusk-network/styles/tailwind.css";
+import Cta from "./Cta.svelte";
 
 export default {
-  title: "Components/Organisms/CTA",
+  title: `components/organisms/Cta`,
+  component: Cta,
   parameters: {
-    notes: readme,
+    layout: "centered",
   },
-  decorators: [withKnobs],
+  argTypes: {},
 };
 
-export const Default = () => ({
-  Component: DefaultView,
+const Template = (args) => ({
+  Component: Default,
   props: {
-    heading: text("CTA Heading", "Call to action heading..."),
-    href: text("CTA Link", "#"),
-    buttonText: text("CTA Button Text", "The cake is a lie"),
+    ...args,
   },
 });
+
+export const Basic = Template.bind({});
+Basic.args = {
+  buttonText: "This is a call to Action button",
+  heading: "I'm a heading",
+  href: "https://dusk.network/",
+};
