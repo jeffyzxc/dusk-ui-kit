3fae412d745df18e46498806f4c7ade4c379cddc
Essam Dawood <essam_d@hotmail.com>
2021-04-28 12:42:53 +0200

    docs(button): 📚️ added the test file
    affects: @dusk-network/button
    
    commented out the code because of an error saying that it doesn't recognise the test or expect
    functions

--- packages/atoms/button/Button.test.js
+++ packages/atoms/button/Button.test.js
@@ -1,9 +1,20 @@
-function sum(a, b) {
-  return a + b;
-}
-describe("Sample Test", () => {
-  it("should return 3 as the result of the function", () => {
-    // set timeout to prevent false positives with tests
-    expect(sum(1, 2)).toBe(3);
-  });
-});
+// function sum(a, b) {
+//   return a + b;
+// }
+// describe("Sample Test", () => {
+//   it("should return 3 as the result of the function", () => {
+//     // set timeout to prevent false positives with tests
+//     expect(sum(1, 2)).toBe(3);
+//   });
+// });
+
+// import { render } from '@testing-library/svelte';
+// import Button from './Button.svelte';
+// import { Basic } from './Button.stories';
+
+// test("Should generate basic button", () => {
+//   const { container } = render(Button, {
+//     props: Basic.args,
+//   });
+//   expect(container.contains('Basic')).toBe(true);
+// });
