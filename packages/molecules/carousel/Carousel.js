733519c7ec520c37e142ce2aeb938321b95c2397
Essam Dawood <essam_d@hotmail.com>
2021-04-30 17:36:41 +0200

    docs(carousel): 📚️ renamed the file deleting test so it doesn't break
    affects: @dusk-network/carousel

--- packages/molecules/carousel/Carousel.js
+++ packages/molecules/carousel/Carousel.js
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/svelte";
 import { Basic } from "./Carousel.stories";
-import Carousel from "./storybook-views/Default.svelte";
+import * as Carousel from "./storybook-views/Default.svelte";
 
 test("shows proper img", () => {
   const results = render(Carousel, { props: Basic.args });
