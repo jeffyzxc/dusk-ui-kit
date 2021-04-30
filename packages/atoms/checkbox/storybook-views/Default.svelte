a60f0412cd4c753f5487d9d200c4a3c32976e61d
Essam Dawood <essam_d@hotmail.com>
2021-04-30 17:34:29 +0200

    docs(checkbox): 📚️ added aria-checked and data-testid attr
    affects: @dusk-network/checkbox

--- packages/atoms/checkbox/storybook-views/Default.svelte
+++ packages/atoms/checkbox/storybook-views/Default.svelte
@@ -1,7 +1,18 @@
 <script>
   import Checkbox from "../Checkbox.svelte";
   export let variant;
+  export let label1;
+  export let label2;
 </script>
 
-<Checkbox variant="{variant}" name="check_2" id="check_1">Yo! I am a Checkbox</Checkbox>
-<Checkbox variant="{variant}" name="check_2" id="check_2">Yo! I am a Checkbox too</Checkbox>
+<Checkbox role="checkbox" aria-checked="true" variant="{variant}" name="check_2" id="check_1"
+  >{label1}</Checkbox
+>
+<Checkbox
+  data-testid="checkbox-result"
+  role="checkbox"
+  aria-checked="false"
+  variant="{variant}"
+  name="check_2"
+  id="check_2">{label2}</Checkbox
+>
