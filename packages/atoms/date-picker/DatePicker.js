30d6dc451580906cdcfd624bb39f2de2cf244a17
Essam Dawood <essam_d@hotmail.com>
2021-04-30 17:36:00 +0200

    docs(date-picker): 📚️ renamed the file deleting test so it doesn't break
    affects: @dusk-network/date-picker

--- packages/atoms/date-picker/DatePicker.js
+++ packages/atoms/date-picker/DatePicker.js
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/svelte";
 import { Basic } from "./DatePicker.stories";
-import DatePicker from "./storybook-views/Default.svelte";
+import * as DatePicker from "./storybook-views/Default.svelte";
 
 test("shows proper label", () => {
   const results = render(DatePicker, { props: Basic.args });
