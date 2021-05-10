4ce76d0e4d63586de146a02379e8cf829fd826f0
Kieran Hall <kieran.hall@gmail.com>
2021-05-11 00:03:06 +0200

    fix(container): 🐛 restored container config
    affects: @dusk-network/styles

--- packages/styles/dusk.tailwind.config.js
+++ packages/styles/dusk.tailwind.config.js
@@ -137,6 +137,16 @@ module.exports = {
           900: "#4A400D",
         },
       },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
       fontFamily: {
         sans: [
           "Hind Siliguri",
