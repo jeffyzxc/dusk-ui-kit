2bee6a4d32a14a55c7b91c781e574467cf2ac0d0
Kieran Hall <kieran.hall@gmail.com>
2021-07-25 18:13:28 +0200

    feat(alert): ✨ updated Alert styles
    affects: @dusk-network/alert

--- packages/molecules/alert/Alert.stories.js
+++ packages/molecules/alert/Alert.stories.js
@@ -1,10 +1,24 @@
 import Default from "./storybook-views/Default.svelte";
 import Alert from "@dusk-network/alert";
+import { variants } from "@dusk-network/helpers";
 
 export default {
   title: `Components/Molecules/Alert`,
   component: Alert,
-  argTypes: {},
+  argTypes: {
+    variant: {
+      control: {
+        type: "select",
+        options: Object.values(variants.MOLECULE.ALERT),
+      },
+    },
+    dismissable: {
+      control: {
+        type: "boolean",
+        options: [true, false],
+      },
+    },
+  },
 };
 
 const Template = (args) => ({
