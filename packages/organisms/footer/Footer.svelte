524894e6de10d8c05f2fb857adaa7cf039674b9f
Kieran Hall <kieran.hall@gmail.com>
2021-07-30 14:49:11 +0200

    perf(footer): ⚡️ cleaning up the footer component footprint
    affects: @dusk-network/footer

--- packages/organisms/footer/Footer.svelte
+++ packages/organisms/footer/Footer.svelte
@@ -1,44 +1,19 @@
 <script>
-  import { current_component } from "svelte/internal";
-  import { forwardEventsBuilder, useActions, variants, types } from "@dusk-network/helpers";
+  import { types } from "@dusk-network/helpers";
   import Minimal from "./Minimal.svelte";
   import Maximal from "./Maximal.svelte";
   import "./styles.css";
 
-  let className = "";
-  export { className as class };
-  export let variant = variants.ORGANISM.FOOTER.BRAND;
   export let type = types.ORGANISM.FOOTER.MINIMAL;
-  export let use = [];
   export let social;
   export let categories;
 
   let today = new Date();
   let year = today.getFullYear();
   const copyright = `© 2018 - ${year} Dusk Network B.V. All Rights Reserved.`;
-
-  const forwardEvents = forwardEventsBuilder(current_component);
-
-  function getClassNames(variant) {
-    let classNames = "";
-    switch (variant) {
-      case variants.ORGANISM.FOOTER.BRAND:
-        classNames += " duk-footer--brand";
-        break;
-      default:
-        classNames += "";
-    }
-
-    return classNames;
-  }
 </script>
 
-<footer
-  use:useActions="{use}"
-  use:forwardEvents
-  class="duk-footer {className} {getClassNames(variant)}"
-  {...$$restProps}
->
+<footer class="{$$props.class || ''} duk-footer">
   {#if type === types.ORGANISM.FOOTER.MAXIMAL}
     <Maximal categories="{categories}" />
   {/if}
