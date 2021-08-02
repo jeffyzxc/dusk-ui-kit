881816592154c2ef7bfa4798600bde4288a98ba5
Kieran Hall <kieran.hall@gmail.com>
2021-08-02 16:31:44 +0200

    refactor(shadow): ♻️ applied shadow config uniformly
    affects: @dusk-network/button, @dusk-network/card, @dusk-network/drop-down,
    @dusk-network/progress-bar, @dusk-network/table, @dusk-network/text-field, @dusk-network/alert,
    @dusk-network/styles

--- packages/styles/dusk.tailwind.config.cjs
+++ packages/styles/dusk.tailwind.config.cjs
@@ -11,31 +11,21 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      backgroundImage: (theme) => ({
+        check: "url(./icons/checkbox.svg)",
+      }),
       boxShadow: {
-        DEFAULT: "0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)",
-        md: " 0 4px 6px -1px rgba(0, 0, 0, .1), 0 2px 4px -1px rgba(0, 0, 0, .06)",
-        lg: " 0 10px 15px -3px rgba(0, 0, 0, .1), 0 4px 6px -2px rgba(0, 0, 0, .05)",
-        xl: " 0 20px 25px -5px rgba(0, 0, 0, .1), 0 10px 10px -5px rgba(0, 0, 0, .04)",
-        "2xl": "0 25px 50px -12px rgba(0, 0, 0, .25)",
-        inner: "inset 0 2px 4px 0 rgba(0,0,0,0.06)",
-        outline: "0 0 0 3px rgba(66,153,225,0.5)",
-        "active-t": "0 -2px 0 rgba(245, 245, 245, 1);",
-        "active-r": "2px 0 0 rgba(245, 245, 245, 1);",
-        "active-t-purple": "0 -2px 0 #3b218c",
-        "active-r-purple": "2px 0 0 #3b218c;",
         marketing: "1px 1px 0px #D82E60;",
         "marketing-hover": "8px 8px 0px #D82E60;",
         none: "none",
         card: "0px 3px 6px rgba(0, 0, 0, .161)",
         switch: "0px 3px 6px rgba(0, 0, 0, .161)",
         table: "0px 3px 6px rgba(0, 0, 0, .161)",
+        alert: "0px 3px 6px rgba(0, 0, 0, .161)",
       },
       borderRadius: {
         none: "0",
-        sm: ".1875rem",
-        DEFAULT: ".375rem",
-        lg: ".5625rem",
-        "3xl": "1.375rem",
+        xl: "1.375rem",
         full: "9999px",
       },
       colors: {
