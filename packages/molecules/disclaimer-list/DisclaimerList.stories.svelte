3d0350a6b3b19a3d5fd4273b929df02e1e9b0f0f
deuch13 <panturu.alexandru@gmail.com>
2021-09-14 15:51:20 +0300

    feat(disclaimerlist): new disclaimer list component

--- packages/molecules/disclaimer-list/DisclaimerList.stories.svelte
+++ packages/molecules/disclaimer-list/DisclaimerList.stories.svelte
@@ -1,6 +1,8 @@
 <script>
   import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
-  import DisclaimerList from "@dusk-network/disclaimer-list";
+  import DisclaimerList, { Item } from "@dusk-network/disclaimer-list";
+  import Icon from "@dusk-network/icon";
+  import RichText from "@dusk-network/rich-text";
 </script>
 
 <Meta
@@ -10,10 +12,45 @@
   argTypes="{{}}"
 />
 
-<Template let:args>
- 
-  
+<Template let:args>  
+  <DisclaimerList>
+    <Item>
+      <svelte:fragment slot="icon">
+        <Icon name="alert-outline" size="xl" variant="danger"/>
+      </svelte:fragment>
+      <RichText>
+        <p>Warning</p>
+      </RichText>
+    </Item>
 
+    <Item type='{2}'>
+      <svelte:fragment slot="icon">
+        <Icon name="safe" size="xxxl" variant="danger"/>
+      </svelte:fragment>
+      <RichText size="sm">
+        <h2>Don't Lose It</h2>
+        <p>If you lose your keystore file, your wallet will be lost.</p>
+      </RichText>
+    </Item>
+    <Item type='{2}'>
+      <svelte:fragment slot="icon">
+        <Icon name="eye-outline" size="xxxl" variant="danger"/>
+      </svelte:fragment>
+      <RichText size="sm">
+        <h2>Don't Share It</h2>
+        <p>Anyone with your keystore file can access your funds.<br> Beware of malicious phishing sites.</p>
+      </RichText>
+    </Item>
+    <Item type='{2}'>
+      <svelte:fragment slot="icon">
+        <Icon name="content-save-alert-outline" size="xxxl" variant="danger"/>
+      </svelte:fragment>
+      <RichText size="sm">
+        <h2>Store It Securely</h2>
+        <p>Secure it like the milions of dollars it may one day be worth</p>
+      </RichText>
+    </Item>
+  </DisclaimerList>
 </Template>
 
 <Story name="Default" args="{{}}" />
--- packages/molecules/disclaimer-list/Item.svelte
+++ packages/molecules/disclaimer-list/Item.svelte
@@ -1,4 +1,10 @@
-<script></script>
-<div class="duk-disclaimer-list__item">
+<script>
+    export let type;
+</script>
+<div 
+    class="duk-disclaimer-list__item"
+    class:duk-disclaimer-list__item--gray = "{type === 2}"
+>
+    <slot name="icon"/>
     <slot/>
 </div>
