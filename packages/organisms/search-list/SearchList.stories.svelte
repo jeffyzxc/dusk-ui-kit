7c5c1d67a1fa163bd2b74997b0eabf079ebab24f
deuch13 <panturu.alexandru@gmail.com>
2021-09-15 14:47:01 +0300

    feat(search-list): add new search list component

---
+++ packages/organisms/search-list/SearchList.stories.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import SearchList from "@dusk-network/search-list";
+</script>
+
+<Meta
+  title="Components/Organisms/Search List"
+  component="{SearchList}"
+  parameters="{{ layout: 'padded' }}"
+  argTypes="{{
+    multiselect: {
+      table: {
+        disable: true,
+      },
+    },
+  }}"
+/>
+
+<Template let:args>
+  <SearchList/>
+</Template>
+
+<Story name="Default" args="{{}}" />
---
+++ packages/organisms/search-list/SearchList.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { data } from "./stores/data.js";
+  import "./styles.css";
+
+ 
+</script>
+
+<div
+  class="{$$props.class || ''} duk-search-list"
+>
+  <slot />
+</div>
