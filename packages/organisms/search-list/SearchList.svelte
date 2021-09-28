e9e77ff166fa5cf87dd206fda48b3087a024d517
Kieran Hall <kieran.hall@gmail.com>
2021-09-28 20:33:58 +0200

    :ambulance:  patching up search-list
    disabled search-list story as getting an unexpected lint error

--- packages/organisms/search-list/SearchList.svelte
+++ packages/organisms/search-list/SearchList.svelte
@@ -1,13 +1,12 @@
 <script>
-  import {onMount, onDestroy} from "svelte";
+  import { onMount, onDestroy } from "svelte";
   import { store, searchTerm, searchResults } from "./stores/store.js";
   import Control from "@dusk-network/control";
   import TextField from "@dusk-network/text-field";
   import Button from "@dusk-network/button";
   import Icon from "@dusk-network/icon";
-  import { Item } from "@dusk-network/search-list";
+  import Item from "./Item.svelte";
   import "./styles.css";
-
 
   export let data = [];
   let searchValue;
@@ -16,25 +15,25 @@
     store.set(data);
   });
 
-  onDestroy(() => { store.reset()});
-  
-  function onClick(){
+  onDestroy(() => {
+    store.reset();
+  });
+
+  function onClick() {
     searchTerm.set(searchValue);
   }
 </script>
 
-<div
-  class="{$$props.class || ''} duk-search-list"
->
+<div class="{$$props.class || ''} duk-search-list">
   <Control width="full">
     <Button slot="buttonPostfix" on:click="{onClick}">
-      <Icon name="magnify" size="sm"/>
+      <Icon name="magnify" size="sm" />
     </Button>
-    <TextField placeholder="Search" bind:value="{searchValue}"/>
+    <TextField placeholder="Search" bind:value="{searchValue}" />
   </Control>
   {#each $searchResults as token}
     <Item token="{token}">
-      <slot name='results'/>
+      <slot name="results" />
     </Item>
   {/each}
 </div>
