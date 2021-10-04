5fe01f89b36d19b4c05f3e20cace5b0ef0866240
Kieran Hall <kieran.hall@gmail.com>
2021-10-04 10:30:40 +0200

    :pencil:  adding docs prep

--- meta/index.cjs
+++ meta/index.cjs
@@ -1,21 +1,27 @@
 const path = require("path");
 const doc = require("svelte-docster");
 const glob = require("tiny-glob/sync");
-
 const packages = path.resolve("../packages");
 const output = {};
 const fileSearch = glob("**/*([a-zA-Z]).svelte", { cwd: packages });
+
 fileSearch.forEach((file) => {
   const filename = path.basename(file, ".svelte");
   const component = path.dirname(file).split(path.sep)[1];
+  const group = path.dirname(file).split(path.sep)[0];
+
   if (filename.startsWith("_")) return;
-  if (!output[component]) output[component] = {};
+  if (!output[group]) output[group] = {};
+  if (!output[group][component]) output[group][component] = {};
+
   const fullFilePath = path.join(packages, file);
-  output[component][filename] = Object.assign(doc({ file: fullFilePath }), {
+
+  output[group][component][filename] = Object.assign(doc({ file: fullFilePath }), {
     all: undefined,
     module: undefined,
   });
-  // console.log(`Generated docs for ${component}/${filename}`);
+
+  console.log(`Generated docs for ${group}/${component}/${filename}`);
 });
 
 module.exports = output;
