6c7b91945b6297e5b1d19bed1ea2a1128184b786
deuch13 <panturu.alexandru@gmail.com>
2021-10-04 15:20:42 +0300

    :ambulance:  fixed Jest config

--- packages/atoms/drop-down/DropDown.test.js
+++ packages/atoms/drop-down/DropDown.test.js
@@ -1,11 +1,17 @@
-import "@testing-library/jest-dom/extend-expect";
+/**
+ * @jest-environment jsdom
+ */
+import "@testing-library/jest-dom";
 import { render } from "@testing-library/svelte";
-import { Basic } from "./DropDown.stories";
-import DropDown from "@dusk-network/drop-down";
+//import { Basic } from "./DropDown.stories";
+import DropDown from "./DropDown.svelte";
 
 test("it renders", () => {
   const results = render(DropDown, {
-    props: Basic.args,
+    props: { options: ["option 1", "option 2", "option 3", "option 4", "option 5"] },
   });
-  expect(() => results.getByRole(DropDown).toHaveClass);
+  // expect(() => results.getByRole(DropDown).toHaveClass);
+
+  //const element = document.createElement('div');
+  expect(results).not.toBeNull();
 });
