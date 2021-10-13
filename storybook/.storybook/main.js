0c76939d187b521b2b92548fe2da67ce7004d71f
Kieran Hall <kieran.hall@gmail.com>
2021-10-13 11:29:29 +0200

    :green_heart:  trying with live ghpages config

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -28,7 +28,7 @@ module.exports = {
     );
     config.plugins.push(virtualMetaPlugin());
     config.resolve.dedupe = ["@storybook/client-api"]; // 🔧
-    config.publicDir = "assets";
+    config.publicDir = "dusk-ui-kit";
 
     return config;
   },
