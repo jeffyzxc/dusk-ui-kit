b256c97ba67732ba62d76491b15cad7a9c154e2b
deuch13 <panturu.alexandru@gmail.com>
2021-11-02 13:10:47 +0200

    :sparkles:  RichText context for Accordion UI Kit component

--- storybook/stories/organisms/accordion/Accordion.stories.svelte
+++ storybook/stories/organisms/accordion/Accordion.stories.svelte
@@ -4,6 +4,7 @@
   import { withTests } from "@storybook/addon-jest";
   import Accordion, { Item } from "@dusk-network/accordion";
   import Card, { Content } from "@dusk-network/card";
+  import RichText from "@dusk-network/rich-text";
   import meta from "../../meta.js";
 </script>
 
@@ -43,4 +44,15 @@
       </Accordion>
     </Content>
   </Card>
+</Story>
+
+<Story name="Accordion inside RichText" args="{{}}">
+  <RichText>
+    <Accordion>
+      <Item title="First Item">First item content...</Item>
+      <Item title="Second Item">Second item content...</Item>
+      <Item title="Third Item">Third item content...</Item>
+      <Item title="Fourth Item">Fourth item content...</Item>
+    </Accordion>
+  </RichText>
 </Story>
