25b6aafa64543352320566f9fad693d694623868
Kieran Hall <kieran.hall@gmail.com>
2021-11-06 16:44:08 +0100

    :green_heart:  Clean up postchangeset script

--- scripts/postchangeset.cjs
+++ scripts/postchangeset.cjs
@@ -9,13 +9,11 @@ const fs = require("fs");
 const path = require("path");
 const { execSync } = require("child_process");
 const simpleGit = require("simple-git");
-const { exit } = require("process");
 const git = simpleGit({
   config: ["core.hooksPath=/dev/null"], // 🔧 Disables hooks entirely
   baseDir: process.cwd(), // FIXME this should probably be set to the repo root.
 });
 
 const versionFile = JSON.parse(fs.readFileSync(path.resolve("./versions.json"), "utf-8"));
-execSync(`npm version ${versionFile.releases[0].newVersion}`);
+execSync(`npm version --git-tag-version=false ${versionFile.releases[0].newVersion}`);
 git.add("./package.json").commit("🤖 (auto-commit) preparing package version for release");
-exit;
