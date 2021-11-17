ea7aae571dc78bab6981db4a76e0cfff0f4537fa
Kieran Hall <kieran.hall@gmail.com>
2021-11-17 17:32:29 +0100

    :ambulance:  Fix failing Pagination test

--- packages/molecules/pagination/Pagination.test.js
+++ packages/molecules/pagination/Pagination.test.js
@@ -5,7 +5,9 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/svelte";
 import Pagination from "./Pagination.svelte";
 import { data } from "../../../storybook/stories/organisms/table/data";
-import { pageNumber } from "../../organisms/table/stores/state";
+import { writable } from "svelte/store";
+
+const pageNumber = writable(1);
 
 test("Component renders", () => {
   const results = render(Pagination, {
