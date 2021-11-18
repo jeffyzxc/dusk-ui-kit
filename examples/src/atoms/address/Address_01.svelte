454175e0908c2442ad82e87f7294b45675e19c55
deuch13 <panturu.alexandru@gmail.com>
2021-11-18 15:29:34 +0200

    :pencil:  docs example for Address UI Kit component

---
+++ examples/src/atoms/address/Address_01.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Address from "@dusk-network/address";
+
+  let address =
+    "0x66D30033B4E0BAF8970e9c8A0aD1D02Cc3e21115fhkllA9urdrTVbAyQZnwy0JLyvbCVZBHpzfBU87Gy4USFWaA6sZ";
+</script>
+
+<Address variant="brand">
+  <p>{address}</p>
+</Address>
