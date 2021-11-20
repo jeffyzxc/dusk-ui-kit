ac26100371813b54b7a5d20d589dd39432f212ed
Kieran Hall <kieran.hall@gmail.com>
2021-11-20 17:40:25 +0100

    :construction:  Progress explorer homepage template stories

--- packages/templates/explorer-homepage/ExplorerHomepage.svelte
+++ packages/templates/explorer-homepage/ExplorerHomepage.svelte
@@ -1,5 +1,18 @@
 <script>
+  import Template from "../default";
   import "./styles.css";
 </script>
 
-<div class="{$$props.class || ''} duk-explorer-homepage"></div>
+<Template class="{$$props.class || ''} duk-template-explorer-homepage" $$restProps="{$$restProps}">
+  <div class="duk-template-explorer-homepage__control-panel">
+    <slot name="control-panel" />
+  </div>
+  <div class="duk-template-explorer-homepage__latest">
+    <div class="duk-template-explorer-homepage__latest-blocks">
+      <slot name="latest-blocks" />
+    </div>
+    <div class="duk-template-explorer-homepage__latest-blocks">
+      <slot name="latest-transactions" />
+    </div>
+  </div>
+</Template>
---
+++ storybook/stories/templates/explorer-homepage/ExplorerHomepage.stories.svelte
@@ -0,0 +1,69 @@
+<script>
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+  import * as svgGenerator from "@cloudfour/simple-svg-placeholder";
+  // import { variants, states } from "@dusk-network/helpers";
+  import Template from "@dusk-network/explorer-homepage";
+  import results from "../../../../.jest-test-results.json";
+  import { withTests } from "@storybook/addon-jest";
+  import Card, { Content } from "@dusk-network/card";
+  import Table, { Row, Datum } from "@dusk-network/table";
+  // import RichText from "@dusk-network/rich-text";
+  // import Icon from "@dusk-network/icon";
+  // import Heading from "@dusk-network/heading";
+  // import Button, { Group } from "@dusk-network/button";
+  import meta from "../../meta";
+
+  console.log(svgGenerator);
+</script>
+
+<Meta
+  title="Components/Templates/Explorer Homepage"
+  decorators="{[withTests({ results })]}"
+  component="{Template}"
+  parameters="{{
+    layout: 'padded',
+    docs: {
+      source: {
+        type: 'code',
+      },
+    },
+    jest: ['packages/templates/explorer-homepage/ExplorerHomepage.test.js'],
+  }}"
+  argTypes="{meta('templates/explorer-homepage/ExplorerHomepage', {})}"
+/>
+
+<Story name="Loaded State" args="{{}}" let:args>
+  <Template>
+    <svelte:fragment slot="control-panel">
+      <Card>
+        <Content>
+          <img
+            alt=""
+            src="{svgGenerator({
+              width: 180,
+              height: 135,
+              bgColor: '#261457',
+              textColor: '#7FDBFF',
+            })}"
+          />
+        </Content>
+      </Card>
+    </svelte:fragment>
+    <svelte:fragment slot="latest-blocks">
+      <Table>
+        <Row>
+          <Datum>test</Datum>
+          <Datum>testing</Datum>
+        </Row>
+      </Table>
+    </svelte:fragment>
+    <svelte:fragment slot="latest-transactions">
+      <Table>
+        <Row>
+          <Datum>test</Datum>
+          <Datum>testing</Datum>
+        </Row>
+      </Table>
+    </svelte:fragment>
+  </Template>
+</Story>
