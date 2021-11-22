ed2ddc23b21dda3e622e823da2fc02150926d4d7
Kieran Hall <kieran.hall@gmail.com>
2021-11-22 09:58:56 +0100

    :sparkles:  Add Explorer page mock

---
+++ storybook/stories/templates/_Placeholder.svelte
@@ -0,0 +1,22 @@
+<script>
+  import * as svgGenerator from "@cloudfour/simple-svg-placeholder";
+
+  export let width = 1440;
+  export let height = 500;
+  // export let fontSize = 32;
+  export let bgColor = "#261457";
+  export let textColor = "#C1B1ED";
+  export let text = undefined;
+</script>
+
+<img
+  alt="placeholder for component"
+  src="{svgGenerator({
+    width,
+    height,
+    bgColor,
+    textColor,
+    text,
+    // fontSize
+  })}"
+/>
