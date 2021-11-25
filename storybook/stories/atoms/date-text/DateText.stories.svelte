f2a6115bed7b7ad2aed0dbcca6d4a2709ac46845
Kieran Hall <kieran.hall@gmail.com>
2021-11-25 08:14:03 +0100

    :pencil:  Add DateText story

---
+++ storybook/stories/atoms/date-text/DateText.stories.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+  import results from "../../../../.jest-test-results.json";
+  import { withTests } from "@storybook/addon-jest";
+  import DateText from "@dusk-network/date-text";
+  import meta from "../../meta.js";
+</script>
+
+<Meta
+  title="Components/Atoms/Date Text"
+  decorators="{[withTests({ results })]}"
+  parameters="{{
+    layout: 'centered',
+    docs: {
+      source: {
+        type: 'code',
+      },
+    },
+    jest: ['packages/atoms/date-text/DateText.test.js'],
+  }}"
+  component="{DateText}"
+  argTypes="{meta('atoms/date-text/DateText')}"
+/>
+
+<Story name="Default (from now) time" args="{{ time: '2021-01-01 21:03:02 +0000 UTC' }}" let:args>
+  <DateText {...args} />
+</Story>
+
+<Story
+  name="With additional timestamp"
+  args="{{ time: '2021-01-01 21:03:02 +0000 UTC', showTimestamp: true }}"
+  let:args
+>
+  <DateText {...args} />
+</Story>
