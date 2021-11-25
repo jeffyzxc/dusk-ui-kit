f60cc6dcdb4e5700b869a26450962e7bef459b82
Kieran Hall <kieran.hall@gmail.com>
2021-11-25 07:57:16 +0100

    :sparkles:  Add DateText functionality

--- packages/atoms/date-text/DateText.svelte
+++ packages/atoms/date-text/DateText.svelte
@@ -1,5 +1,33 @@
 <script>
   import "./styles.css";
+  import dayjs from "dayjs";
+  import relativeTime from "dayjs/plugin/relativeTime.js";
+  import customParseFormat from "dayjs/plugin/customParseFormat.js";
+
+  /**
+   * The time string that needs to be formatted
+   */
+  export let time = "";
+
+  /**
+   * Show and additional timestamp after the formatted time.
+   */
+  export let showTimestamp = false;
+
+  /**
+   * The format of the time string.
+   * */
+  export let datetimeFormat = "YYYY-MM-DD HH:mm:ss ZZ";
+
+  const TIMESTAMP_FORMAT = "MMM-DD-YYYY HH:mm:ss A ZZ";
+
+  dayjs.extend(relativeTime);
+  dayjs.extend(customParseFormat);
 </script>
 
-<div class="{$$props.class || ''} duk-date-text"></div>
+<time class="{$$props.class || ''} duk-date-text" datetime="{time}" title="{time}">
+  {dayjs(time, datetimeFormat).fromNow()}
+  {#if showTimestamp}
+    <time>({dayjs(time, datetimeFormat).format(TIMESTAMP_FORMAT)})</time>
+  {/if}
+</time>
