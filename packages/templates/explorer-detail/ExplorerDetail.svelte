0fd5a077bbf7e23be5a989f45486a0eec0bd3b97
deuch13 <panturu.alexandru@gmail.com>
2021-12-01 15:27:34 +0200

    :sparkles:  Add Explorer Detail template

--- packages/templates/explorer-detail/ExplorerDetail.svelte
+++ packages/templates/explorer-detail/ExplorerDetail.svelte
@@ -1,5 +1,23 @@
 <script>
+  import Template from "../default";
   import "./styles.css";
 </script>
 
-<div class="{$$props.class || ''} duk-explorer-detail"></div>
+<Template class="{$$props.class || ''} duk-template-explorer-detail" $$restProps="{$$restProps}">
+  <svelte:fragment slot="navbar">
+    <slot name="navbar" />
+  </svelte:fragment>
+  <section class="duk-template__container">
+    <div class="duk-template-explorer-detail__details">
+      <slot name="detail-details" />
+    </div>
+  </section>
+  <section class="duk-template__container">
+    <div class="duk-template-explorer-detail__list">
+      <slot name="detail-list" />
+    </div>
+  </section>
+  <svelte:fragment slot="footer">
+    <slot name="footer" />
+  </svelte:fragment>
+</Template>
