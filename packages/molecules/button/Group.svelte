3a26d0e231b676f5cf93cd4bfdeb27fc01e39596
Kieran Hall <kieran.hall@gmail.com>
2021-12-05 11:36:09 +0100

    :lipstick:  Add alignment props for Button Group

--- packages/molecules/button/Group.svelte
+++ packages/molecules/button/Group.svelte
@@ -1,3 +1,12 @@
-<div class="{$$props.class || ''} duk-button-group">
+<script>
+  export let align = "left";
+</script>
+
+<div
+  class="{$$props.class || ''} duk-button-group"
+  class:duk-button-group--left="{align === 'left'}"
+  class:duk-button-group--center="{align === 'center'}"
+  class:duk-button-group--right="{align === 'right'}"
+>
   <slot />
 </div>
