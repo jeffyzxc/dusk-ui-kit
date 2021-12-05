5f704214478ee0c82a8c26a4f57c9a67c201be06
Kieran Hall <kieran.hall@gmail.com>
2021-12-05 11:41:38 +0100

    :sparkles:  Add alignment control for Card Content

--- packages/organisms/card/Content.svelte
+++ packages/organisms/card/Content.svelte
@@ -1,3 +1,12 @@
-<div class="{$$props.class || ''} duk-card__content">
+<script>
+  export let align = "left";
+</script>
+
+<div
+  class="{$$props.class || ''} duk-card__content"
+  class:duk-card__content--left="{align === 'left'}"
+  class:duk-card__content--center="{align === 'center'}"
+  class:duk-card__content--right="{align === 'right'}"
+>
   <slot />
 </div>
