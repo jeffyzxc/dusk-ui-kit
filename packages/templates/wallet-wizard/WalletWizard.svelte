1b984583a3b57dd9af38b4cf775e717898e3d5d8
deuch13 <panturu.alexandru@gmail.com>
2021-12-06 20:05:16 +0200

    :sparkles:  Wallet Wizard template and story

---
+++ packages/templates/wallet-wizard/WalletWizard.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Template from "../default";
+  import "./styles.css";
+</script>
+
+<Template class="{$$props.class || ''} duk-template-wallet-wizard" $$restProps="{$$restProps}">
+  <svelte:fragment slot="navbar">
+    <slot name="navbar" />
+  </svelte:fragment>
+  <section class="duk-template__container">
+    <div class="duk-template-wallet-wizard__wizard">
+      <slot name="wizard" />
+    </div>
+  </section>
+  <svelte:fragment slot="footer">
+    <slot name="footer" />
+  </svelte:fragment>
+</Template>
---
+++ storybook/stories/templates/wallet/wallet-wizard/WalletWizard.stories.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+  import Template from "@dusk-network/wallet-wizard";
+  // import results from "../../../../../.jest-test-results.json";
+  // import { withTests } from "@storybook/addon-jest";
+  import meta from "../../../meta";
+  import Placeholder from "../../_Placeholder.svelte";
+</script>
+
+<Meta
+  title="Components/Templates/Wallet/Wizard"
+  component="{Template}"
+  parameters="{{
+    layout: 'fullscreen',
+    docs: {
+      source: {
+        type: 'code',
+      },
+    },
+    jest: ['packages/templates/wallet-wizard/WalletWizard.test.js'],
+  }}"
+  argTypes="{meta('templates/wallet-wizard/WalletWizard', {})}"
+/>
+
+<Story name="Loaded State" args="{{}}" let:args>
+  <Template>
+    <svelte:fragment slot="navbar">
+      <Placeholder text="Navbar" width="1920" height="{104}" />
+    </svelte:fragment>
+    <svelte:fragment slot="wizard">
+      <Placeholder text="Wizard" height="{565}" />
+    </svelte:fragment>
+    <svelte:fragment slot="footer">
+      <Placeholder text="Footer" width="1920" height="{108}" />
+    </svelte:fragment>
+  </Template>
+</Story>
