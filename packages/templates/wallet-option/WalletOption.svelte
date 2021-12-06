7d932f66330f73a4f59d1ca35f699cab89c9280c
deuch13 <panturu.alexandru@gmail.com>
2021-12-06 16:15:09 +0200

    :sparkles:  Wallet Options template and story

---
+++ packages/templates/wallet-option/WalletOption.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Template from "../default";
+  import "./styles.css";
+</script>
+
+<Template class="{$$props.class || ''} duk-template-wallet-option" $$restProps="{$$restProps}">
+  <svelte:fragment slot="navbar">
+    <slot name="navbar" />
+  </svelte:fragment>
+  <section class="duk-template__container">
+    <div class="duk-template-wallet-option__heading-mobile">
+      <slot name="heading-mobile" />
+    </div>
+  </section>
+  <section class="duk-template__container">
+    <div class="duk-template-wallet-option__block">
+      <div class="duk-template-wallet-option__lead-question">
+        <slot name="lead-question" />
+      </div>
+      <div class="duk-template-wallet-option__option">
+        <slot name="option" />
+      </div>
+    </div>
+  </section>
+  <svelte:fragment slot="footer">
+    <slot name="footer" />
+  </svelte:fragment>
+</Template>
