8f72b1506885d0be37c4e3381a03513324b31071
deuch13 <panturu.alexandru@gmail.com>
2021-12-13 13:04:39 +0200

    :pencil:  QR Code example

---
+++ examples/src/atoms/qr-code/QrCode_01.svelte
@@ -0,0 +1,8 @@
+<script>
+  //   import QrCode from "@dusk-network/qr-code";
+
+  //   let address =
+  //     "0x66D30033B4E0BAF8970e9c8A0aD1D02Cc3e21115fhkllA9urdrTVbAyQZnwy0JLyvbCVZBHpzfBU87Gy4USFWaA6sZ";
+</script>
+
+<!-- <QrCode value="{address}" /> -->
