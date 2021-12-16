3d072f1e8b461541e6b486ac806805b92c557eb3
deuch13 <panturu.alexandru@gmail.com>
2021-12-16 18:05:13 +0200

    :sparkles:  Storybook Skeleton Loader story

---
+++ storybook/stories/molecules/skeleton-loader/SkeletonLoader.stories.svelte
@@ -0,0 +1,35 @@
+<script>
+  // TODO Clean up content in template etc.
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+  import { variants } from "@dusk-network/helpers";
+  // import results from "../../../../.jest-test-results.json";
+  // import { withTests } from "@storybook/addon-jest";
+  import SkeletonLoader from "@dusk-network/skeleton-loader";
+  import meta from "../../meta.js";
+</script>
+
+<Meta
+  title="Components/Molecules/Skeleton Loader"
+  component="{SkeletonLoader}"
+  parameters="{{
+    layout: 'centered',
+    docs: {
+      source: {
+        type: 'code',
+      },
+    },
+    jest: ['packages/molecules/skeleton-loader/SkeletonLoader.test.js'],
+  }}"
+  argTypes="{meta('molecules/skeleton-loader/SkeletonLoader', {
+    variant: {
+      options: Object.values(variants.SKELETON_LOADER),
+      control: {
+        type: 'select',
+      },
+    },
+  })}"
+/>
+
+<Story name="Default Skeleton Loader" args="{{ height: '300' }}" let:args>
+  <SkeletonLoader {...args} />
+</Story>
