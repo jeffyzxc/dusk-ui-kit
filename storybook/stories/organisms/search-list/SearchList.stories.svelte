153443fe81efbfac61579d2446bbddfe3dfdf77c
deuch13 <panturu.alexandru@gmail.com>
2021-12-21 17:25:57 +0200

    :recycle:  Storybook SearchList stories with contextual store

--- storybook/stories/organisms/search-list/SearchList.stories.svelte
+++ storybook/stories/organisms/search-list/SearchList.stories.svelte
@@ -5,8 +5,9 @@
   import SearchList from "@dusk-network/search-list";
   import RichText from "@dusk-network/rich-text";
   import { tokens } from "./data.js";
-  import { searchResults } from "@dusk-network/search-list/stores/store.js";
   import meta from "../../meta.js";
+
+  let searchResults;
 </script>
 
 <Meta
@@ -20,45 +21,53 @@
 />
 
 <Story name="Default" args="{{ data: tokens }}" let:args>
-  <SearchList data="{args.data}">
-    {#each $searchResults as token}
-      <div class="duk-search-list__item">
-        <div class="duk-search-list__item--token">
-          <div class="duk-search-list__item--border">
-            {#if token.image}
-              <img src="{token.image}" alt="symbol" />
-            {/if}
+  <SearchList data="{args.data}" bind:dataSearchResults="{searchResults}">
+    {#if searchResults}
+      {#each $searchResults as token}
+        <div class="duk-search-list__item">
+          <div class="duk-search-list__item--token">
+            <div class="duk-search-list__item--border">
+              {#if token.image}
+                <img src="{token.image}" alt="symbol" />
+              {/if}
+            </div>
+            <RichText>
+              <p>{token.token}</p>
+            </RichText>
           </div>
           <RichText>
-            <p>{token.token}</p>
+            <p>{token.amount}</p>
           </RichText>
         </div>
-        <RichText>
-          <p>{token.amount}</p>
-        </RichText>
-      </div>
-    {/each}
+      {/each}
+    {/if}
   </SearchList>
 </Story>
 
 <Story name="Search List with button" args="{{ data: tokens, fieldButton: true }}" let:args>
-  <SearchList data="{args.data}" fieldButton="{args.fieldButton}">
-    {#each $searchResults as token}
-      <div class="duk-search-list__item">
-        <div class="duk-search-list__item--token">
-          <div class="duk-search-list__item--border">
-            {#if token.image}
-              <img src="{token.image}" alt="symbol" />
-            {/if}
+  <SearchList
+    data="{args.data}"
+    fieldButton="{args.fieldButton}"
+    bind:dataSearchResults="{searchResults}"
+  >
+    {#if searchResults}
+      {#each $searchResults as token}
+        <div class="duk-search-list__item">
+          <div class="duk-search-list__item--token">
+            <div class="duk-search-list__item--border">
+              {#if token.image}
+                <img src="{token.image}" alt="symbol" />
+              {/if}
+            </div>
+            <RichText>
+              <p>{token.token}</p>
+            </RichText>
           </div>
           <RichText>
-            <p>{token.token}</p>
+            <p>{token.amount}</p>
           </RichText>
         </div>
-        <RichText>
-          <p>{token.amount}</p>
-        </RichText>
-      </div>
-    {/each}
+      {/each}
+    {/if}
   </SearchList>
 </Story>
