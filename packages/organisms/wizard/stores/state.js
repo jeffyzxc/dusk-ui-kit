5b083f3bf6a5789fc6e10be8b082411e74231a10
deuch13 <panturu.alexandru@gmail.com>
2021-12-21 12:40:21 +0200

    :fire:  Remove Wizard commented code

--- packages/organisms/wizard/stores/state.js
+++ packages/organisms/wizard/stores/state.js
@@ -2,14 +2,6 @@ import { writable } from "svelte/store";
 import { getContext, setContext } from "svelte";
 import { key } from "../key.js";
 
-// const store = writable(1);
-
-// export const step = {
-//   subscribe: store.subscribe,
-//   increment: () => store.update((store) => store + 1),
-//   decrement: () => store.update((store) => store - 1),
-// };
-
 function getStep() {
   const getStore = () => {
     const store = writable(1);
