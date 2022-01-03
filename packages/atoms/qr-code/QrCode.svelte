88e21c93191fcb1d5e28f9faadd6db5778378420
Kieran Hall <kieran.hall@gmail.com>
2022-01-03 10:59:54 +0100

    :bug:  Fix ssr issue in qr code component

--- packages/atoms/qr-code/QrCode.svelte
+++ packages/atoms/qr-code/QrCode.svelte
@@ -1,6 +1,6 @@
 <script>
   import "./styles.css";
-  import QRCode from "qrcode";
+  import * as QRCode from "qrcode";
   import { onMount } from "svelte";
 
   /**
@@ -30,6 +30,7 @@
   $: bgColor, renderQr();
 
   function renderQr() {
+    if (typeof window === "undefined") return;
     QRCode.toDataURL(value, { width: width, color: { dark: qrColor, light: bgColor } })
       .then((url) => {
         src = url;
