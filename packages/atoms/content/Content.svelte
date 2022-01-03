bc3662d0c1beab176d258aa972a388a5034d8aed
Kieran Hall <kieran.hall@gmail.com>
2022-01-03 17:54:51 +0100

    :construction:  Add Content atom

---
+++ packages/atoms/content/Content.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "./styles.css";
+  import { getContext } from "svelte";
+  import contexts from "@dusk-network/helpers/contexts.js";
+
+  const context = getContext("DUK:content:context");
+</script>
+
+<div
+  class="{$$props.class || ''} duk-content"
+  class:duk-content--card="{context === contexts.CONTENT.CARD}"
+>
+  <slot />
+</div>
