abe9fea3ca73fb88e02128acefb29c81d8d1fa5d
Kieran Hall <kieran.hall@gmail.com>
2022-01-06 15:59:07 +0100

    :sparkles:  Add DetailList component

---
+++ packages/molecules/detail-list/DetailList.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { setContext, getContext } from "svelte";
+  import contexts from "@dusk-network/helpers/contexts.js";
+  import "./styles.css";
+
+  const context = getContext("DUK:detail-list:context");
+
+  setContext("DUK:icon:context", contexts.ICON.DETAIL_LIST);
+</script>
+
+<dl
+  class="{$$props.class || ''} duk-detail-list"
+  class:duk-detail-list--card="{context === contexts.DETAIL_LIST.CARD}"
+>
+  <slot />
+</dl>
---
+++ packages/molecules/detail-list/index.js
@@ -0,0 +1,5 @@
+import DetailList from "./DetailList.svelte";
+import Item from "./Item.svelte";
+
+export default DetailList;
+export { Item };
