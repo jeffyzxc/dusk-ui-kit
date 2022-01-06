62fda7236ffeb2413ed050a84bd83ffd42b6f8cf
deuch13 <panturu.alexandru@gmail.com>
2022-01-06 21:37:36 +0200

    :lipstick:  Update WalletAccessKeystore page story

--- storybook/stories/pages/wallet/wallet-access-keystore/WalletAccessKeystore.stories.svelte
+++ storybook/stories/pages/wallet/wallet-access-keystore/WalletAccessKeystore.stories.svelte
@@ -10,11 +10,13 @@
   import Content from "@dusk-network/content";
   import Wizard, { Step } from "@dusk-network/wizard";
   import Button from "@dusk-network/button";
+  import Group from "@dusk-network/group";
   import Form from "@dusk-network/form";
   import Control from "@dusk-network/control";
   import TextField from "@dusk-network/text-field";
   import FileUpload from "@dusk-network/file-upload";
   import ProgressBar from "@dusk-network/progress-bar";
+  import Icon from "@dusk-network/icon";
   import * as yup from "yup";
   import meta from "../../../meta";
 
@@ -111,35 +113,38 @@
                   }
                 }}"
               >
+                <Icon name="keystore-file-outline" viewbox="0 0 28 35" size="xxl" />
                 <Control width="full" name="file" let:id let:state>
                   <FileUpload
+                    class="keystore-login__file"
                     id="{id}"
                     state="{state}"
                     uploaded="{uploaded}"
                     on:inputFile="{inputChange}"
                   />
                 </Control>
-
-                {#if !uploaded}
-                  <Button
-                    class="keystore-login__cta"
-                    variant="cta"
-                    size="base"
-                    disabled="{!isFileLoaded}"
-                    type="submit">Upload</Button
-                  >
-                {/if}
-                {#if uploaded}
-                  <Button
-                    class="keystore-login__cta"
-                    variant="cta"
-                    size="base"
-                    on:click="{() => {
-                      next();
-                      uploaded = false;
-                    }}">Continue</Button
-                  >
-                {/if}
+                <Group align="center">
+                  {#if !uploaded}
+                    <Button
+                      class="keystore-login__cta"
+                      variant="cta"
+                      size="base"
+                      disabled="{!isFileLoaded}"
+                      type="submit">Upload</Button
+                    >
+                  {/if}
+                  {#if uploaded}
+                    <Button
+                      class="keystore-login__cta"
+                      variant="cta"
+                      size="base"
+                      on:click="{() => {
+                        next();
+                        uploaded = false;
+                      }}">Continue</Button
+                    >
+                  {/if}
+                </Group>
               </Form>
             </Step>
             <Step number="{2}">
@@ -154,7 +159,14 @@
                   }
                 }}"
               >
-                <Control width="full" name="password" let:id let:state>
+                <Icon name="keystore-file-outline" viewbox="0 0 28 35" size="xxl" />
+                <Control
+                  class="keystore-login__control"
+                  width="full"
+                  name="password"
+                  let:id
+                  let:state
+                >
                   <TextField
                     id="{id}"
                     state="{state}"
@@ -163,10 +175,9 @@
                     placeholder="Password"
                   />
                 </Control>
-
-                <Button class="keystore-login__cta" type="submit" variant="cta"
-                  >Access My Wallet</Button
-                >
+                <Group align="center">
+                  <Button type="submit" variant="cta">Access My Wallet</Button>
+                </Group>
               </Form>
             </Step>
           </Wizard>
