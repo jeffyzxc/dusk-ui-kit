454a70f86df3ae112d3f90dc0d6987b9a684e59d
Kieran Hall <kieran.hall@gmail.com>
2022-01-06 16:01:43 +0100

    :pencil:  Add Chip component story

--- packages/atoms/chip/Chip.svelte
+++ packages/atoms/chip/Chip.svelte
@@ -2,4 +2,8 @@
   import "./styles.css";
 </script>
 
-<div class="{$$props.class || ''} duk-chip"></div>
+<div class="{$$props.class || ''} duk-chip">
+  <div class="duk-chip__text">
+    <span><slot /></span>
+  </div>
+</div>
---
+++ storybook/stories/atoms/chip/Chip.stories.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+  // import results from "../../../../.jest-test-results.json";
+  // import { withTests } from "@storybook/addon-jest";
+  import { variants } from "@dusk-network/helpers";
+  import Chip from "@dusk-network/chip";
+  import meta from "../../meta.js";
+</script>
+
+<Meta
+  title="Components/Atoms/Chip"
+  parameters="{{
+    layout: 'centered',
+    docs: {
+      source: {
+        type: 'code',
+      },
+    },
+    jest: ['packages/atoms/address/Address.test.js'],
+  }}"
+  component="{Chip}"
+  argTypes="{meta('atoms/address/Address', {
+    variant: {
+      options: Object.values(variants.ADDRESS),
+      control: {
+        type: 'select',
+      },
+    },
+  })}"
+/>
+
+<Story name="Default chip" args="{{}}" let:args>
+  <Chip {...args}>Round Leader</Chip>
+</Story>
