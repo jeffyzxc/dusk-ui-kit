843ceab5e89f882126ec646fe6836bd54d097f45
deuch13 <panturu.alexandru@gmail.com>
2022-01-10 20:51:30 +0200

    :pencil:  UI Kit TruncateText docs example

---
+++ examples/src/atoms/truncate-text/TruncateText_01.svelte
@@ -0,0 +1,7 @@
+<script>
+  import TruncateText from "@dusk-network/truncate-text";
+
+  let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+</script>
+
+<TruncateText width="half" text="{text}" />
