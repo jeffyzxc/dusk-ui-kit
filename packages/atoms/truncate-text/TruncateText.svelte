55fb30c03ae0a6f1b65a946604b7a865802bf037
deuch13 <panturu.alexandru@gmail.com>
2022-01-10 19:26:40 +0200

    :sparkles:  Add new truncate method to UI Kit TruncateText

--- packages/atoms/truncate-text/TruncateText.svelte
+++ packages/atoms/truncate-text/TruncateText.svelte
@@ -1,10 +1,12 @@
 <script>
   import "./styles.css";
   import { widths } from "@dusk-network/helpers";
+  import { getContext } from "svelte";
+  import contexts from "@dusk-network/helpers/contexts.js";
 
   /**
-   * Sets the width of the Control inside current container.
-   * @type { "full" | "half" | "quarter" }
+   * Sets the width of the component inside current container.
+   * @type { "max" | "three-quarters" | "half" | "quarter" | "one-third"}
    */
   export let width = widths.TRUNCATE_TEXT.HALF;
 
@@ -12,6 +14,33 @@
    * Sets title attribute of the truncated text.
    */
   export let title = "";
+
+  /**
+   * Sets the truncate method to js slice and removes css truncate.
+   */
+  export let text = "";
+
+  let ratio;
+  switch (width) {
+    case widths.TRUNCATE_TEXT.QUARTER:
+      ratio = 4;
+      break;
+    case widths.TRUNCATE_TEXT.ONE_THIRD:
+      ratio = 3;
+      break;
+    case widths.TRUNCATE_TEXT.HALF:
+      ratio = 2;
+      break;
+    case widths.TRUNCATE_TEXT.THREE_QUARTERS:
+      ratio = 0.75;
+      break;
+    default:
+      ratio = 2;
+  }
+
+  $: truncateText = text.slice(0, text.length / ratio) + "...";
+
+  const context = getContext("DUK:truncate-text:context");
 </script>
 
 <div
@@ -21,8 +50,13 @@
   class:duk-truncate-text--half="{width === widths.TRUNCATE_TEXT.HALF}"
   class:duk-truncate-text--three-quarters="{width === widths.TRUNCATE_TEXT.THREE_QUARTERS}"
   class:duk-truncate-text--max="{width === widths.TRUNCATE_TEXT.MAX}"
+  class:duk-truncate-text--datum="{context === contexts.TRUNCATE_TEXT.DATUM}"
 >
   <span class="duk-truncate-text__value" title="{title}">
-    <slot />
+    {#if text.length > 0}
+      {truncateText}
+    {:else}
+      <slot />
+    {/if}
   </span>
 </div>
