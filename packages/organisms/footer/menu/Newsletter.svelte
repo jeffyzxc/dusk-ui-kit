57563b429a15466d263666bd0b76147605ab5e8b
Kieran Hall <kieran.hall@gmail.com>
2022-01-25 23:25:51 +0100

    :bug:  Make changes outlined in #325

--- packages/organisms/footer/menu/Newsletter.svelte
+++ packages/organisms/footer/menu/Newsletter.svelte
@@ -1,10 +1,17 @@
 <script>
   import TextField from "@dusk-network/text-field";
   import Button from "@dusk-network/button";
+  import Control from "@dusk-network/control";
+  import Form from "@dusk-network/form";
+  import Icon from "@dusk-network/icon";
 </script>
 
-<form class="duk-footer__newsletter">
+<Form class="duk-footer__newsletter">
   <h3 class="duk-footer__title">Subscribe to our newsletter</h3>
-  <TextField name="email" id="email" placeholder="Enter your email address" />
-  <Button variant="cta">Subscribe</Button>
-</form>
+  <Control width="full" buttonPostfix="{true}">
+    <TextField name="email" id="email" placeholder="Enter your email address" />
+    <Button variant="cta" slot="buttonPostfix">
+      <Icon name="email-outline" />
+    </Button>
+  </Control>
+</Form>
