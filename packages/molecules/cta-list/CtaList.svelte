b8361a1ecbc0e99461d0213fde606c403fe50758
deuch13 <panturu.alexandru@gmail.com>
2022-02-03 10:56:44 +0200

    :recycle:  change variant CTALIST to CTA_LIST

--- packages/molecules/cta-list/CtaList.svelte
+++ packages/molecules/cta-list/CtaList.svelte
@@ -1,6 +1,5 @@
 <script>
   // FIXME numerical types are unclear.  Use helper for constants.
-  // TODO CTALIST needs to be renamed to CTA_LIST.
   import variants from "@dusk-network/helpers/variants.js";
   import "./styles.css";
 
@@ -8,7 +7,7 @@
    * Sets the variant of the CTA List.
    * @type {"brand" | "cta" | "info" | "success" | "warning" | "danger" }
    */
-  export let variant = variants.CTALIST.BRAND;
+  export let variant = variants.CTA_LIST.BRAND;
 
   /**
    * Sets the type of the CTA List.
@@ -19,12 +18,12 @@
 <ol
   class="{$$props.class || ''} duk-cta-list"
   class:duk-cta-list--counter="{type === 2}"
-  class:duk-cta-list--brand="{variant === variants.CTALIST.BRAND}"
-  class:duk-cta-list--cta="{variant === variants.CTALIST.CTA}"
-  class:duk-cta-list--info="{variant === variants.CTALIST.INFO}"
-  class:duk-cta-list--success="{variant === variants.CTALIST.SUCCESS}"
-  class:duk-cta-list--warning="{variant === variants.CTALIST.WARNING}"
-  class:duk-cta-list--danger="{variant === variants.CTALIST.DANGER}"
+  class:duk-cta-list--brand="{variant === variants.CTA_LIST.BRAND}"
+  class:duk-cta-list--cta="{variant === variants.CTA_LIST.CTA}"
+  class:duk-cta-list--info="{variant === variants.CTA_LIST.INFO}"
+  class:duk-cta-list--success="{variant === variants.CTA_LIST.SUCCESS}"
+  class:duk-cta-list--warning="{variant === variants.CTA_LIST.WARNING}"
+  class:duk-cta-list--danger="{variant === variants.CTA_LIST.DANGER}"
 >
   <slot />
 </ol>
