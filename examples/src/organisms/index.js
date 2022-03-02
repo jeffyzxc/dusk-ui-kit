9de80614d8f72a944f529237cabcfe7b4a60836f
deuch13 <panturu.alexandru@gmail.com>
2022-03-02 19:58:45 +0200

    :pencil:  docs example for UI Kit ErrorBlock

--- examples/src/organisms/index.js
+++ examples/src/organisms/index.js
@@ -1,5 +1,6 @@
 import * as Card from "./card";
 import * as CookieBanner from "./cookie-banner";
+import * as ErrorBlock from "./error-block";
 import * as Wizard from "./wizard";
 import * as Accordion from "./accordion";
 import * as Footer from "./footer";
@@ -9,4 +10,16 @@ import * as Navbar from "./navbar";
 import * as SearchList from "./search-list";
 import * as Table from "./table";
 
-export { Card, CookieBanner, Wizard, Accordion, Footer, Form, Hero, Navbar, SearchList, Table };
+export {
+  Card,
+  CookieBanner,
+  ErrorBlock,
+  Wizard,
+  Accordion,
+  Footer,
+  Form,
+  Hero,
+  Navbar,
+  SearchList,
+  Table,
+};
