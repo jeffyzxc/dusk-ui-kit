3de7e3ebb030f22d11a73733b9e2d918e6af27be
deuch13 <panturu.alexandru@gmail.com>
2022-03-16 16:45:21 +0200

    :pencil:  new UI Kit Icon docs example

--- examples/src/atoms/icon/index.js
+++ examples/src/atoms/icon/index.js
@@ -1,3 +1,4 @@
 import Icon_01 from "./Icon_01.svelte";
 import Icon_02 from "./Icon_02.svelte";
-export { Icon_01, Icon_02 };
+import Icon_03 from "./Icon_03.svelte";
+export { Icon_01, Icon_02, Icon_03 };
